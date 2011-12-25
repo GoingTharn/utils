f6851a81538f012cf66f3d02841c6afc2a4f622e
John Barry <john.barry@gmail.com>
2011-12-25 10:06:22 -0600

    Fixed parse args

--- VimTools.py
+++ VimTools.py
@@ -4,7 +4,7 @@ import subprocess
 import sys
 
 def vim_ezadd(gitUrl, bundlePath=None):
-    if bundlePath == None:
+    if bundlePath is None:
         if sys.platform == 'win32':
             bundlePath = os.path.join('~', 'vimfiles', 'bundle')
         else:
@@ -17,17 +17,19 @@ def vim_ezadd(gitUrl, bundlePath=None):
     subprocess.call(cmd)
     os.chdir(start_dir)
     
-def vim_load_many(urlList, bundlePath):
+def vim_load_many(urlList, bundlePath=None):
     for url in urlList:
         vim_ezadd( url, bundlePath)
 
-def main(*args):
-    url = args[0]
-    vim_ezadd(url)
+def main(**kw):
+    url = kw.get('url')
+    bundle = kw.get('bundle')
+    vim_ezadd(url, bundle)
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description="File to add")
     parser.add_argument('--url',  help='Url for the git repo to clone')
+    parser.add_argument('--bundle',  help='path to pathogen bundle directory')
     args = parser.parse_args()
 
-    main(args.url)
+    main(url=args.url, bundle=args.bundle)
